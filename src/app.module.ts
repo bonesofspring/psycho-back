f3b596c1980e532af05ce66546bca321f15f209c
Ivan Volchenkov <bonesofspring@gmail.com>
2024-09-15 10:30:56 +0300

    feat: docker setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExperimentModule } from './experiment/experiment.module';
+import { ResultModule } from './result/result.module';
+import { AttemptModule } from './attempt/attempt.module';
+import { UserModule } from './user/user.module';
+import { PatientModule } from './patient/patient.module';
 
 @Module({
-  imports: [],
+  imports: [ExperimentModule, ResultModule, AttemptModule, UserModule, PatientModule],
   controllers: [AppController],
   providers: [AppService],
 })

5cac1f6cb98c6dc0e98e34c3cbd4ce1d80833d9a
Ivan Volchenkov <bonesofspring@gmail.com>
2024-09-15 10:38:16 +0300

    feat: postgres setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,27 @@ import { ResultModule } from './result/result.module';
 import { AttemptModule } from './attempt/attempt.module';
 import { UserModule } from './user/user.module';
 import { PatientModule } from './patient/patient.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ExperimentModule, ResultModule, AttemptModule, UserModule, PatientModule],
+  imports: [
+    ExperimentModule,
+    ResultModule,
+    AttemptModule,
+    UserModule,
+    PatientModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
